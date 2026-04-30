ee3a4fccb874098cf1b5875f42fc070587c939f2
Kim Ji Hyeon <117696182+Hyeoniss@users.noreply.github.com>
2026-04-30 20:35:08 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/team202ok/demo/Team202okApplication.java
@@ -0,0 +1,13 @@
+package com.team202ok.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team202okApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team202okApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team202ok/demo/Team202okApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team202ok.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team202okApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
