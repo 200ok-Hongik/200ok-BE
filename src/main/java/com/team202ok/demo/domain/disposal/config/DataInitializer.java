fe8f64156b4ab3390f956c3d8f68270ac4828e14
Kim Ji Hyeon <117696182+Hyeoniss@users.noreply.github.com>
2026-09-22 14:50:54 +0900

    feat: 품목코드 수정

--- src/main/java/com/team202ok/demo/domain/disposal/config/DataInitializer.java
+++ src/main/java/com/team202ok/demo/domain/disposal/config/DataInitializer.java
@@ -7,8 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-
-//추후 품목 완전 확정시 data.sql로 변환!!!!
+// 추후 품목 완전 확정시 data.sql로 변환!!!!
 @Component
 @Order(0)
 @RequiredArgsConstructor
@@ -17,25 +16,28 @@ public class DataInitializer implements CommandLineRunner {
     private final TrashCategoryRepository trashCategoryRepository;
 
     @Override
-    public void run(String... args) throws Exception {
-        // 서버가 켜질 때 한 번 실행되는 곳입니다.
+    public void run(String... args) {
 
-        // 데이터가 비어있을 때만 기초 데이터를 넣습니다. (중복 저장 방지)
-        if (trashCategoryRepository.count() == 0) {
-            TrashCategory petBottle = TrashCategory.builder()
-                    .code("PET_BOTTLE")
-                    .name("페트병")
-                    .build();
+        saveIfNotExists("CLEAR_PET_BOTTLE", "무색 페트병");
+        saveIfNotExists("PLASTIC", "플라스틱");
+        saveIfNotExists("CAN", "캔류");
+        saveIfNotExists("GLASS_BOTTLE", "유리병");
+        saveIfNotExists("VINYL", "비닐");
+        saveIfNotExists("PAPER", "종이");
+        saveIfNotExists("PAPER_PACK", "종이팩");
+        saveIfNotExists("STYROFOAM", "스티로폼");
 
-            TrashCategory can = TrashCategory.builder()
-                    .code("CAN")
-                    .name("캔류")
-                    .build();
+        System.out.println("✅ 기초 마스터 데이터(품목)가 등록되었습니다!");
+    }
 
-            trashCategoryRepository.save(petBottle);
-            trashCategoryRepository.save(can);
-
-            System.out.println("✅ 기초 마스터 데이터(품목)가 성공적으로 등록되었습니다!");
+    private void saveIfNotExists(String code, String name) {
+        if (trashCategoryRepository.findByCode(code).isEmpty()) {
+            trashCategoryRepository.save(
+                    TrashCategory.builder()
+                            .code(code)
+                            .name(name)
+                            .build()
+            );
         }
     }
 }
